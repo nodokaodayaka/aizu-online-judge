195f3b516bd5048f36e429388072d8b3eceda15f
nodokaodayaka <wankosob@gmail.com>
2020-06-10 13:11:42 +0900

    clear

--- ALDS1_7/ALDS1_7_C.py
+++ ALDS1_7/ALDS1_7_C.py
@@ -3,45 +3,53 @@ n = int(input())
 
 preorder_list = []
 def preorder(node):
-  preorder_list.append(node[ID])
-  if node[LEFT] != -1:
-    preorder(nodes[node[LEFT]])
-  if node[RIGHT] != -1:
-    preorder(nodes[node[RIGHT]])
+  preorder_list.append(node[0][ID])
+  if node[0][LEFT] != -1:
+    preorder(nodes[node[0][LEFT]])
+  if node[0][RIGHT] != -1:
+    preorder(nodes[node[0][RIGHT]])
 
 inorder_list = []
 def inorder(node):
-  if node[LEFT] != -1:
-    inorder(nodes[node[LEFT]])
-  inorder_list.append(node[ID])
-  if node[RIGHT] != -1:
-    inorder(nodes[node[RIGHT]])
+  if node[0][LEFT] != -1:
+    inorder(nodes[node[0][LEFT]])
+  inorder_list.append(node[0][ID])
+  if node[0][RIGHT] != -1:
+    inorder(nodes[node[0][RIGHT]])
 
 postorder_list = []
 def postorder(node):
-  if node[LEFT] != -1:
-    postorder(nodes[node[LEFT]])
-  if node[RIGHT] != -1:
-    postorder(nodes[node[RIGHT]])
-  postorder_list.append(node[ID])
+  if node[0][LEFT] != -1:
+    postorder(nodes[node[0][LEFT]])
+  if node[0][RIGHT] != -1:
+    postorder(nodes[node[0][RIGHT]])
+  postorder_list.append(node[0][ID])
 
-nodes = []
-for _ in range(n):
+nodes = [[] for _ in range(n)]
+for i in range(n):
   id, left, right = list(map(int, input().split()))
-  nodes.append({ID:id, LEFT:left, RIGHT:right})
+  nodes[id].append({ID:id, LEFT:left, RIGHT:right})
 
-root = {}
+id_list = [0 for _ in range(n)]
 for node in nodes:
-  if node[ID] == 0:
-    root = node
+  if node[0][LEFT] != -1:
+    id_list[node[0][LEFT]] += 1
+  if node[0][RIGHT] != -1:
+    id_list[node[0][RIGHT]] += 1
+
+root_id = -1
+for i,v in enumerate(id_list):
+  if v == 0:
+    root_id = i
     break
 
-preorder(root)
-inorder(root)
-postorder(root)
+
+preorder(nodes[root_id])
 print ('Preorder')
-print (' '.join(map(str, preorder_list)))
+print (' '+' '.join(map(str, preorder_list)))
+inorder(nodes[root_id])
 print ('Inorder')
-print (' '.join(map(str, inorder_list)))
+print (' '+' '.join(map(str, inorder_list)))
+postorder(nodes[root_id])
 print ('Postorder')
-print (' '.join(map(str, postorder_list)))
+print (' '+' '.join(map(str, postorder_list)))
